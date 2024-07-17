3a82e0b0ca02b966ac2df6fd7cd6bedb3d3a02e7
阮家友 <1439120442@qq.com>
2024-07-17 18:04:17 +0800

    develop

---
+++ src/models/manager/schedule.ts
@@ -0,0 +1,51 @@
+import mongoose, { Model } from "mongoose";
+import Base from '../../base.js';
+import { } from '../../@types/types.js'
+
+export interface ISchedule {
+  _id: string;
+  name: string;
+  desc: string;
+  script: string;
+  status: number;
+  createdAt: Date;
+}
+
+class Schedule extends Base<ISchedule> {
+  constructor(db: mongoose.Connection, params: { methods?: { [key: string]: Function }, statics?: { [key: string]: (this: Model<ISchedule>) => any } } = {}) {
+    super();
+    const schema = new mongoose.Schema({
+      _id: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      desc: {
+        type: String,
+        default: '',
+      },
+      status: {
+        type: Number,
+        default: 1,
+      },
+      script: {
+        type: String
+      },
+      createdAt: {
+        type: Date,
+      },
+    }, {
+      strict: false,
+      versionKey: false,
+      excludeIndexes: true,
+      collection: 'Schedule',
+      methods: params.methods || {},
+      statics: params.statics || {},
+    });
+    this.model = db.model<ISchedule>('Schedule', schema);
+    Base.models.Schedule = this.model;
+  }
+}
+
+export default Schedule;
